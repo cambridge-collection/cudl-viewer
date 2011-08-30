97c2b2de8bd64266c22c9affa976da60de0634c7
jlf44 <jlf44@59899196-baf2-4bc4-a94d-4a08719e171f>
2011-08-30 10:24:02 +0000

    adding in jetty plugin for testing and bringing in the Spring + Spring MVC framework.
    git-svn-id: https://source.caret.cam.ac.uk/svn/projects/ul-foundations/foundationsviewer/trunk@9361 59899196-baf2-4bc4-a94d-4a08719e171f

--- src/main/java/ulcambridge/foundations/viewer/DocumentViewController.java
+++ src/main/java/ulcambridge/foundations/viewer/DocumentViewController.java
@@ -1,53 +1,23 @@
 package ulcambridge.foundations.viewer;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-/**
- * Reads in the URL specified by the newtonDoc parameter and filters the content
- * to only display what we want.
- * 
- * @author jennie
- * 
- */
-public class DocumentViewController extends HttpServlet {
-	
-   private static final transient Log log = 
-		    LogFactory.getLog(DocumentViewController.class);
-	  
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+import java.io.IOException;
 
-		doPost(request, response);
+public class DocumentViewController implements Controller {
 
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		logger.info("Returning index view");
+		System.out.println("stdout - Returning index view");
+
+		return new ModelAndView("/WEB-INF/document-view.jsp");
 	}
-
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		/* Read in the part of the url structure that contains the document id */
-		String documentId = request.getRequestURI();
-		
-		/* Read in the part of the url structure that contains the page number */
-		log.debug("requestURI: "+request.getRequestURI());
-		log.debug("servlet Path: "+request.getServletPath());
-		
-		/* FIXME Redirect to view with appropriate parameters */
-		
-		String destination = "/"+request.getServletPath()+"/document-view.jsp";
-		 
-		//RequestDispatcher rd = getServletContext().getRequestDispatcher(destination);
-		//rd.forward(request, response);
-		response.sendRedirect(destination);
-		
-	}
-	
 }
