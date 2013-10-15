873aa89790b596ae304ad9dfc2279deafd03a2f1
gmt30 <gmt30@59899196-baf2-4bc4-a94d-4a08719e171f>
2013-10-15 16:05:29 +0000

    Factor out the mock json reader into a separate class
    git-svn-id: https://source.caret.cam.ac.uk/svn/projects/ul-foundations/foundationsviewer/trunk@20630 59899196-baf2-4bc4-a94d-4a08719e171f

--- src/test/java/ulcambridge/foundations/viewer/DocumentViewControllerTest.java
+++ src/test/java/ulcambridge/foundations/viewer/DocumentViewControllerTest.java
@@ -1,16 +1,9 @@
 package ulcambridge.foundations.viewer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -78,36 +71,4 @@ public class DocumentViewControllerTest extends TestCase {
 		
 	}
 	
-	/**
-	 * Instead of connecting to a URL for the JSON data, just returns set
-	 * Object.
-	 * 
-	 * @author jennie
-	 * 
-	 */
-	private class MockJSONReader extends JSONReader {
-
-		@Override
-		public JSONObject readJsonFromUrl(String url) throws IOException,
-				JSONException {
-
-			ClassLoader loader = Thread.currentThread().getContextClassLoader();
-			InputStream file = loader.getResourceAsStream("MS-ADD-04004.json");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(file));
-			StringBuilder builder = new StringBuilder();
-			String line;
-			while ((line = reader.readLine()) != null) {
-				builder.append(line);
-			}
-			reader.close();
-			JSONObject json = new JSONObject(builder.toString());
-			
-			return json;
-		}
-		public boolean urlExists(String urlString) {
-
-			return true;
-
-		}
-	}	
 }
