e3a9d40ae91654032d80a758b08bb07099fb8ad9
jlf44 <jlf44@cam.ac.uk>
2014-12-08 14:58:14 +0000

    Moving email to use Amazon's Simple Email Service. NOTE THIS CHANGE REQUIRES A cudl-global.properties UPDATE.

--- src/main/java/ulcambridge/foundations/viewer/FormController.java
+++ src/main/java/ulcambridge/foundations/viewer/FormController.java
@@ -1,5 +1,7 @@
 package ulcambridge.foundations.viewer;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
@@ -8,8 +10,6 @@ import net.tanesha.recaptcha.ReCaptchaResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.commons.mail.EmailException;
-import org.apache.commons.mail.MultiPartEmail;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import ulcambridge.foundations.viewer.forms.FeedbackForm;
-import ulcambridge.foundations.viewer.forms.MailingListForm;
 import ulcambridge.foundations.viewer.model.Properties;
 
 @Controller
@@ -30,8 +29,6 @@ public class FormController {
 			.getString("feedbackEmail");
 	protected final static String feedbackSubject = Properties
 			.getString("feedbackSubject");
-	protected final static String feedbackHost = Properties
-			.getString("feedbackHost");
 
 	@RequestMapping(method = RequestMethod.GET, value = "/feedbackform.html")
 	public ModelAndView showForm(FeedbackForm feedbackForm) {
@@ -42,7 +39,8 @@ public class FormController {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/feedbackform.html")
 	public ModelAndView processSubmit(@Valid FeedbackForm feedbackForm,
-			BindingResult result, HttpServletRequest request) throws EmailException {
+			BindingResult result, HttpServletRequest request)
+			throws AddressException, MessagingException {
 
 		// validation for standard parameters
 		if (result.hasErrors()) {
@@ -50,66 +48,43 @@ public class FormController {
 			modelAndView.addObject("errors", result);
 			return modelAndView;
 		}
-		
+
 		// validation for capcha
-        String remoteAddr = request.getRemoteAddr();
-        ReCaptchaImpl reCaptcha = new ReCaptchaImpl();
-        reCaptcha.setPrivateKey("6Lfp19cSAAAAACgXgRVTbk1m11OdFS8sttohEMDv");
+		String remoteAddr = request.getRemoteAddr();
+		ReCaptchaImpl reCaptcha = new ReCaptchaImpl();
+		reCaptcha.setPrivateKey("6Lfp19cSAAAAACgXgRVTbk1m11OdFS8sttohEMDv");
 
-        String challenge = request.getParameter("recaptcha_challenge_field");
-        String uresponse = request.getParameter("recaptcha_response_field");
-        ReCaptchaResponse reCaptchaResponse = reCaptcha.checkAnswer(remoteAddr, challenge, uresponse);
-        
-        if (!reCaptchaResponse.isValid()) {
-        	ObjectError error = new ObjectError("FeedbackForm", "The recaptcha input was not correct, please try again");
-        	result.addError(error);
+		String challenge = request.getParameter("recaptcha_challenge_field");
+		String uresponse = request.getParameter("recaptcha_response_field");
+		ReCaptchaResponse reCaptchaResponse = reCaptcha.checkAnswer(remoteAddr,
+				challenge, uresponse);
+
+		if (!reCaptchaResponse.isValid()) {
+			ObjectError error = new ObjectError("FeedbackForm",
+					"The recaptcha input was not correct, please try again");
+			result.addError(error);
 			ModelAndView modelAndView = new ModelAndView("jsp/feedback");
-			modelAndView.addObject("errors", result);
-			return modelAndView;
-        }
-		
-		
-		// send email with comment in.
-		// Create the email message
-		MultiPartEmail email = new MultiPartEmail();
-		email.setHostName(feedbackHost);
-		email.addTo(feedbackEmail);
-		email.setFrom(feedbackForm.getEmail());
-		email.setSubject(feedbackSubject);
-		email.setMsg("Feedback from the user '" + feedbackForm.getName()
-				+ "' (" + feedbackForm.getEmail() + "):\n\n"
-				+ feedbackForm.getComment());
-
-		// send the email
-		email.send();
-
-		ModelAndView modelAndView = new ModelAndView("jsp/feedback-success");
-		return modelAndView;
-
-	}
-	
-	@RequestMapping(method = RequestMethod.GET, value = "/mailinglistform.html")
-	public ModelAndView showForm(MailingListForm mailingListForm) {
-		ModelAndView modelAndView = new ModelAndView("jsp/mailinglist");
-		modelAndView.addObject("feedbackForm", mailingListForm);
-		return modelAndView;
-	}
-
-	@RequestMapping(method = RequestMethod.POST, value = "/mailinglistform.html")
-	public ModelAndView processSubmit(@Valid MailingListForm mailingListForm,
-			BindingResult result) throws EmailException {
-
-		if (result.hasErrors()) {
-			ModelAndView modelAndView = new ModelAndView("jsp/mailinglist");
 			modelAndView.addObject("errors", result);
 			return modelAndView;
 		}
 
-		// TODO sign up to mailing list.
+		// send email with comment in.
+		boolean success = EmailHelper.sendEmail(
+				feedbackEmail,
+				feedbackEmail,
+				feedbackSubject,
+				"Feedback from the user '" + feedbackForm.getName() + "' ("
+						+ feedbackForm.getEmail() + "):\n\n"
+						+ feedbackForm.getComment());
 
+		if (success) {
+			ModelAndView modelAndView = new ModelAndView("jsp/feedback-success");
+			return modelAndView;
+		} else {
+			ModelAndView modelAndView = new ModelAndView("jsp/feedback-failure");
+			return modelAndView;
+		}
 
-		ModelAndView modelAndView = new ModelAndView("jsp/feedback-success");
-		return modelAndView;
+	}
 
-	}	
 }
