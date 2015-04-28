ca8316695437e4327d69524497c442f820a1c022
jlf44 <jlf44@cam.ac.uk>
2015-04-28 16:13:55 +0100

    Implementing migration of old users bookmarks to new username using google. (from OpenId 2.0 to oauth2).

--- src/main/java/ulcambridge/foundations/viewer/authentication/LoginController.java
+++ src/main/java/ulcambridge/foundations/viewer/authentication/LoginController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.URI;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,6 +19,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Controller;
@@ -26,6 +30,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import ulcambridge.foundations.viewer.dao.BookmarkDao;
+import ulcambridge.foundations.viewer.exceptions.TooManyBookmarksException;
+import ulcambridge.foundations.viewer.model.Bookmark;
+
 @Controller
 public class LoginController {
 
@@ -33,6 +41,7 @@ public class LoginController {
 	private final OAuth2RestOperations googleTemplate;
 	private final OAuth2RestOperations facebookTemplate;
 	private UsersDao usersDao;
+	private BookmarkDao bookmarkDao;
 
 	@Autowired
 	public LoginController(OAuth2RestOperations googleTemplate, OAuth2RestOperations facebookTemplate) {
@@ -44,6 +53,11 @@ public class LoginController {
 	public void setUsersDao(UsersDao usersDao) {
 		this.usersDao = usersDao;
 	}
+	
+	@Autowired
+	public void setBookmarkDao(BookmarkDao bookmarkDao) {
+		this.bookmarkDao = bookmarkDao;
+	}	
 
 	// on path /auth/login/
 	@RequestMapping(value = "/login")
@@ -98,6 +112,9 @@ public class LoginController {
 		// setup user in Spring Security and DB
 		setupUser(usernameEncoded, emailEncoded, session);
 
+		// This should only be called up until Jan 2017. 
+		migrateGoogleUser(usernameEncoded);		
+		
 		// forward to /mylibrary/
 		response.sendRedirect("/mylibrary/");
 
@@ -166,7 +183,7 @@ public class LoginController {
 	 * @param id
 	 * @param session
 	 */
-	public void setupUser(String username, String email, HttpSession session) {
+	private void setupUser(String username, String email, HttpSession session) {
 		
 		// Create user in database if required, and store details in user session.
 		User user = usersDao.createUser(username, email);
@@ -176,5 +193,53 @@ public class LoginController {
 		Authentication auth = new PreAuthenticatedAuthenticationToken(username, null,
 				AuthorityUtils.commaSeparatedStringToAuthorityList(StringUtils.join(user.getUserRoles(), ",")));
 		SecurityContextHolder.getContext().setAuthentication(auth);
+	}
+	
+	
+	/**
+	 * Gets the old google id for this user and moves any bookmarks over to the new
+	 * username.  Note this will stop working Jan 2017 when google stops proving the 
+	 * openid_id. 
+	 *  
+	 * @param username
+	 * @throws JSONException
+	 */
+	private void migrateGoogleUser(String username) throws JSONException {
+		
+		// Does this user have an old OpenID 2.0 id?
+		String openid_id = null;
+		Map<String, Object> map = googleTemplate.getAccessToken().getAdditionalInformation();
+		if (map.containsKey("id_token")) {
+			Object idTokenEncoded = map.get("id_token");
+			Jwt id_token = JwtHelper.decode(idTokenEncoded.toString());
+			JSONObject json = new JSONObject(id_token.getClaims());
+			
+			if (json.has("openid_id")) {
+			  openid_id = json.get("openid_id").toString();
+			}
+		}
+		if (openid_id==null) { return;	}
+		
+		// Does this old user id have any bookmarks?
+		List<Bookmark> oldBookmarks = bookmarkDao.getByUsername(openid_id);
+		for (int i=0; i<oldBookmarks.size(); i++) {
+			Bookmark bookmark = oldBookmarks.get(i);
+			
+			// change old bookmark to new username
+			bookmark.setUsername(username);
+			
+			// add bookmark back to database with new username
+			// and delete from DB using the old username.
+			try {
+				bookmarkDao.add(bookmark);
+				bookmarkDao.delete(openid_id, bookmark.getItemId(), bookmark.getPage());
+				
+			} catch (TooManyBookmarksException e) {
+				// this should not occur
+				e.printStackTrace();
+			}			
+			
+		}
+	
 	}
 }
