3e9a1811a760e3aea0ff743d75d5f900a0d016eb
jlf44 <jlf44@cam.ac.uk>
2015-06-24 12:00:53 +0100

    Making the file saving code transactional.

--- src/main/java/ulcambridge/foundations/viewer/ContentEditorController.java
+++ src/main/java/ulcambridge/foundations/viewer/ContentEditorController.java
@@ -38,34 +38,19 @@ public class ContentEditorController {
 	protected final Log logger = LogFactory.getLog(getClass());
 	
 	// TODO method level security
-	// TODO make transaction using Task Manager
 	// on path /editor/update/
 	@RequestMapping(value = "/update/")
-	public ModelAndView handleUpdateRequest(HttpServletResponse response,
+	public synchronized ModelAndView handleUpdateRequest(HttpServletResponse response,
 			@Valid @ModelAttribute() WriteParameters writeParams,
 	        BindingResult errors ) throws IOException, JSONException {
 		
 		if (errors.hasErrors()) {
 	        throw new IOException("invalid request params");
 	    }
-		
-		// read in data from request
-		String html = writeParams.getHtml();        
-		String filename = writeParams.getFilename();
-		String contentPath = Properties.getString("cudl-viewer-content.path");						
-		
-		// write to file system and rename
-		File file = new File(contentPath, filename + ".new"); // append .new which will be removed after copy completes.
-		InputStream is = new ByteArrayInputStream( html.getBytes( "UTF-8" ) );
-		Files.copy(is, file.toPath());	
-		file.renameTo(new File(contentPath, filename));
-			
-		// commit to git
-		//TODO 
-		
-		// Write response out in JSON. 
+					
+		boolean success = saveHTML(writeParams);
 		JSONObject json = new JSONObject();
-		json.put("writesuccess", true);
+		json.put("writesuccess", success);		
 		
 		response.setContentType("application/json");
 		PrintStream out = null;
@@ -86,9 +71,173 @@ public class ContentEditorController {
 		return null;
 	
 	}
+	
+	
+	/**
+	 * Note: Does not validate params.  This should occur before calling this 
+	 * method. 
+	 * 
+	 * @return
+	 * @throws IOException 
+	 */
+	private boolean saveHTML(WriteParameters writeParams) throws IOException {
+		
+  		// Read in data from request
+		String html = writeParams.getHtml();        
+		String filename = writeParams.getFilename();
+		String contentPath = Properties.getString("cudl-viewer-content.path");						
+		
+		// Setup variables
+		boolean task1done = false, task2done = false, task3done = false, task4done = false, task5done = false;					
+		File newFile = new File(contentPath, filename + ".new").getCanonicalFile(); 
+		InputStream is = new ByteArrayInputStream( html.getBytes( "UTF-8" ) );
+		File parentDir = new File(newFile.getParent()).getCanonicalFile();
+		File existingParentDir = existingParent(parentDir);
+		File oldFile = new File(contentPath, filename + ".old").getCanonicalFile();
+		File file = new File(contentPath, filename).getCanonicalFile();
+		
+		try {
+			
+	      // Task 1 - Create directory (if needed).  		  
+		  parentDir.mkdirs();
+		  task1done = true;		 		  
+		  	
+		  // Task 2 - Write file to file system.
+		  Files.copy(is, newFile.toPath());	
+		  task2done = true;		  		
+		  
+		  // Task 3 - Copy existing file (if available) for rollback.
+		  if (file.exists()) {
+			  Files.copy(file.toPath(), oldFile.toPath());
+		  }
+		  task3done = true;		 
+		  
+		  // Task 4 - rename file (overwriting if exists already).  
+		  newFile.renameTo(file);
+		  task4done = true;		  		  
+		  
+		  // Task 5 - Commit to git
+		  // TODO 
+		  task5done = true;		  		  	
+		  
+		  // Clean up.  
+		  if (oldFile.exists()) { oldFile.delete(); }
+		  
+		  // Write response out in JSON. 		  
+		  return true;
+			
+		} catch (Exception e) {
+						
+			e.printStackTrace();
+			return false; 
+			
+        } finally {
+        	
+            if (! task5done) {
+                if (task4done) {
+                    // Rollback task 4
+                	Files.copy(file.toPath(), newFile.toPath());
+                	if (oldFile.exists()) { oldFile.renameTo(file); } 
+                	else { file.delete(); } 
+                    task4done = false;
+                }
+            }          	
+            if (! task4done) {
+                if (task3done) {
+                    // Rollback task 3
+                	if (oldFile.exists()) { oldFile.delete(); }
+                    task3done = false;
+                }
+            }        	
+            if (! task3done) {
+                if (task2done) {
+                    // Rollback task 2
+                	if (newFile.exists()) { newFile.delete(); } // NB: Possible to lose any .new File that was overwritten
+                    task2done = false;
+                }
+            }
+            if (! task2done) {
+                if (task1done) {
+                	// Rollback task 1 - Creating the parent directory(ies). 
+                	deleteAllDirectoriesUpTo(parentDir, existingParentDir);
+                    task1done= false;
+                }
+            }
+        }			
+	}
+	
+	/**
+	 * Gets lowest level directory that already exists on file system. 
+	 *  
+	 * @param dir
+	 * @return
+	 */
+	private File existingParent(File dir) {
+	    if(dir.exists()) {
+	    	return dir;
+	    }
+	        
+	    while(!dir.exists()) {
+	        dir = dir.getParentFile();
+	    }
+	    
+	    return dir;
+	}
+	
+	/**
+	 * Deletes all directories in the hierarchy up to parentDir
+	 * childDir should be a sub-directory of parentDir.
+	 * Only deletes empty directories.  
+	 * 
+	 * @param parentDir
+	 * @param childDir
+	 * @return
+	 * @throws IOException 
+	 */
+	private boolean deleteAllDirectoriesUpTo(File childDir, File parentDir) throws IOException {
+		
+		childDir = childDir.getCanonicalFile();
+		parentDir = parentDir.getCanonicalFile();
+		
+		if (!childDir.exists() || !parentDir.exists() || !fileIsParent(childDir, parentDir)) {
+			return false;
+		}
+			    
+	    while (!childDir.equals(parentDir)) {
+	    	
+	    	// delete dir if empty. 
+	    	if (childDir.exists() && childDir.isDirectory() && childDir.list().length==0) {
+				childDir.delete();
+				childDir = childDir.getParentFile();
+	    	} else {
+	    		return false;	    		
+	    	}
+	    }
+	    
+	    return true;
+	
+	}
+	
+	private boolean fileIsParent(File child, File parent) throws IOException
+	{
+	    if (!parent.exists() || !parent.isDirectory()) {
+	        return false;
+	    }
+	 
+	    while (child != null) {
+	        if (child.equals(parent)) {
+	            return true;
+	        }
+	        child = child.getParentFile();
+	    }
+	    
+	    // No match found
+	    return false;
+	}
+	
 
 	// Performs validation on parameters used for writing html. 
-	// Do we want html validation? formatting?
+	// TODO Do we want html validation? formatting?
 	public static class WriteParameters {
 
 		@NotNull
