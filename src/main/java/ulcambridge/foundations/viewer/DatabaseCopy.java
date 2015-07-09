0d82d98b8bf18f118c21d8d436d49a33d59e4afc
Rekha <rr494@cam.ac.uk>
2015-07-09 16:51:09 +0100

    Some more database checks and logging added

--- src/main/java/ulcambridge/foundations/viewer/DatabaseCopy.java
+++ src/main/java/ulcambridge/foundations/viewer/DatabaseCopy.java
@@ -19,8 +19,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.log4j.Logger;
 import org.postgresql.copy.CopyManager;
 import org.postgresql.core.BaseConnection;
 
@@ -38,7 +37,9 @@ public class DatabaseCopy {
     private final String pwdLive = Properties.getString("db.jdbc.password.live");
     private final String userDev = Properties.getString("db.jdbc.user.dev");
     private final String pwdDev = Properties.getString("db.jdbc.password.dev");
+    private final String filepath = Properties.getString("db.filepath");
     private CollectionFactory collectionfactory;
+    private static final Logger logger = Logger.getLogger(DatabaseCopy.class.getName());
 
     public DatabaseCopy(CollectionFactory collectionfactory) {
         this.collectionfactory = collectionfactory;
@@ -48,13 +49,13 @@ public class DatabaseCopy {
      copy items,collections and itemsincollection tables from the dev database to a file in /tmp directory
      The files have the same names as the tables
      */
-    public Boolean copyToFile(String tablename, String db) {
-        Boolean success = false;
+    public Boolean copyToFile(String tablename) {
+        Boolean success;
         String url;
-        if (null != db) {
-            url = urlDev;
-            success = writeToFile(tablename, url, "devTables");
-        }
+
+        url = urlDev;
+        success = writeToFile(tablename, url);
+
         return success;
 
     }
@@ -62,12 +63,13 @@ public class DatabaseCopy {
     /*
      Called from copyToFile-dumps the database tables to a file
      */
-    public Boolean writeToFile(String tablename, String url, String folder) {
+    public Boolean writeToFile(String tablename, String url) {
         Connection con = null;
         FileWriter fileWriter = null;
         Boolean success = false;
         int TableRowCount = 0;
         try {
+            //get the no of records in the respective database table
             if ("collections".equals(tablename)) {
                 TableRowCount = this.collectionfactory.getCollectionsRowCount();
 
@@ -78,30 +80,33 @@ public class DatabaseCopy {
                 TableRowCount = this.collectionfactory.getItemsInCollectionsRowCount();
 
             }
-
+            
             con = DriverManager.getConnection(url, userDev, pwdDev);
             CopyManager copyManager = new CopyManager((BaseConnection) con);
-            File file = new File("/tmp/" + folder + "/" + tablename);
+            File file = new File(filepath + tablename);
             fileWriter = new FileWriter(file);
             long copyOutRows = copyManager.copyOut("COPY " + tablename + " TO STDOUT ", fileWriter);
-
+            //the no of copied records == no of database table records?
             success = (copyOutRows > 0) && (copyOutRows == TableRowCount);
+            if (success == false){
+                logger.error("Exception from writeToFile method - failure of (copyOutRows > 0) && (copyOutRows == TableRowCount)");
+            }
             fileWriter.flush();
 
         } catch (SQLException ex) {
-            Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("Exception from writeToFile method - writing from dev db to file", ex);
             success = false;
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("Exception from writeToFile method - writing from dev db to file", ex);
             success = false;
         } catch (IOException ex) {
-            Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("Exception from writeToFile method - writing from dev db to file", ex);
             success = false;
         } catch (NullPointerException ex) {
-            Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("Exception from writeToFile method - writing from dev db to file", ex);
             success = false;
         } catch (Exception ex) {
-            Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("Exception from writeToFile method - writing from dev db to file", ex);
             success = false;
         } finally {
             try {
@@ -114,11 +119,11 @@ public class DatabaseCopy {
                         fileWriter.close();
                     }
                 } catch (IOException ex) {
-                    Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
+                    logger.error("Exception from writing from dev db to file - finally clause - filewriter close ", ex);
                     success = false;
                 }
             } catch (SQLException ex) {
-                Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
+                logger.error("Exception from writing from dev db to file - finally clause - connection close ", ex);
                 success = false;
 
             }
@@ -136,20 +141,23 @@ public class DatabaseCopy {
         try {
 
             CopyManager copyManager = new CopyManager((BaseConnection) con);
-            File file = new File("/tmp/devTables/" + tablename);
+            File file = new File(filepath + tablename);
             FileReader fileReader = new FileReader(file);
             copyManager.copyIn("COPY " + tablename + " FROM STDIN", fileReader);
 
         } catch (SQLException ex) {
-            Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("Exception from writing from file to live db -copy in method", ex);
 
             success = false;
         } catch (FileNotFoundException ex) {
-            Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("Exception from writing from file to live db -copy in method", ex);
 
             success = false;
         } catch (IOException ex) {
-            Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("Exception from writing from file to live db -copy in method", ex);
+            success = false;
+        } catch (Exception ex) {
+            logger.error("Exception from writing from file to live db -copy in method", ex);
 
             success = false;
         }
@@ -177,39 +185,38 @@ public class DatabaseCopy {
 
             //connect to live database
             conlive = DriverManager.getConnection(urlLive, userLive, pwdLive);
+            //make sure the commit is disabled-for rollback
             conlive.setAutoCommit(false);
-            String table;        
+            String table;
 
-            
             //truncate the live tables before copying over the data
             String sql = "TRUNCATE TABLE items,collections,itemsincollection CASCADE";
             PreparedStatement prepareStatement = conlive.prepareStatement(sql);
             int rowsTruncated = prepareStatement.executeUpdate();
-           
             
-
-            //if (rowsTruncated != 0) {
             //iterate over the 3 tables to copy them out from the dev database into a file and then copy them into the live database
             while (iterator.hasNext()) {
                 //get tablename
                 table = iterator.next();
 
-                //write table data to file
-                copyfilesuccess = copyToFile(table, "dev");
+                //write table data from dev db to file
+                copyfilesuccess = copyToFile(table);
 
                 if (copyfilesuccess) {//if copy to file has been successful
                     //copy data from file into live database table
                     dbsuccess = copyIn(table, conlive);
                     if (!dbsuccess) {
                         conlive.rollback();//rollback if any issues
+                        logger.error("Exception from init method-copyIn failure-rollback done");
                         copysuccess = false;
-                        break;//also stop the copy incase of any issues
+                        break;//also stop the copy of remaining tables
 
                     }
-                } else {
+                } else {//copy to file not successful rollback
                     conlive.rollback();//rollback if any issues
+                    logger.error("Exception from init method-copyOut failure-rollback done");
                     copysuccess = false;
-                    break;//also stop the copy incase of any issues
+                    break;//also stop the copy of remaining tables
                 }
             }
             if (!iterator.hasNext() && dbsuccess) {
@@ -218,10 +225,28 @@ public class DatabaseCopy {
             }
 
         } catch (SQLException ex) {
-            Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (Exception ex){
-            Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
-        }finally {
+            logger.error("Exception from init method", ex);
+            try {
+                if (conlive != null) {
+                    conlive.rollback();//rollback if any issues
+                }
+            } catch (SQLException ex1) {
+                logger.error("Exception from init method - conlive.rollback", ex);
+                copysuccess = false;
+            }
+            copysuccess = false;
+        } catch (Exception ex) {
+            logger.error("Exception from init method", ex);
+            try {
+                if (conlive != null) {
+                    conlive.rollback();//rollback if any issues
+                }
+            } catch (SQLException ex1) {
+                logger.error("Exception from init method - conlive.rollback", ex);
+                copysuccess = false;
+            }
+            copysuccess = false;
+        } finally {
             try {
                 if (conlive != null) {
                     conlive.setAutoCommit(true);
@@ -229,7 +254,7 @@ public class DatabaseCopy {
                 }
 
             } catch (SQLException ex) {
-                Logger.getLogger(DatabaseCopy.class.getName()).log(Level.SEVERE, null, ex);
+                logger.error("Exception from init method - finally clause", ex);
             }
         }
 
