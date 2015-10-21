daba8bb509a9eb30407620be4582692deee6b649
Hal Blackburn <hwtb2@cam.ac.uk>
2015-10-21 11:38:59 +0100

    Add the page-collection-organisation ui entry point

--- src/main/java/ulcambridge/foundations/viewer/frontend/PageType.java
+++ src/main/java/ulcambridge/foundations/viewer/frontend/PageType.java
@@ -7,7 +7,8 @@ public enum PageType {
     DOCUMENT("page-document"),
     ADVANCED_SEARCH("page-advancedsearch"),
     TRANSCRIPTION("page-transcription"),
-    LOGIN("page-login");
+    LOGIN("page-login"),
+    COLLECTION_ORGANISATION("page-collection-organisation");
 
     private final String chunkName;
 
