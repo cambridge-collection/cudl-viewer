a7813214323f11adb9b637ff7bc73856ebefd17a
Hal Blackburn <hwtb2@cam.ac.uk>
2015-10-22 16:07:52 +0100

    Add cudlfn:uriEnc() EL function for URI encoding

--- src/main/java/ulcambridge/foundations/viewer/tags/ElFunctions.java
+++ src/main/java/ulcambridge/foundations/viewer/tags/ElFunctions.java
@@ -5,6 +5,9 @@ import ulcambridge.foundations.viewer.ItemFactory;
 import ulcambridge.foundations.viewer.model.Collection;
 import ulcambridge.foundations.viewer.model.Item;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 /**
  * This class defines custom JSP Expression Language (EL) functions required to
  * interact with CUDL's domain classes.
@@ -26,6 +29,25 @@ public final class ElFunctions {
      */
     public static Item getItem(ItemFactory factory, String itemId) {
         return factory.getItemFromId(itemId);
+    }
+
+    /**
+     * Encode a string for inclusion as a component of a URI.
+     *
+     * Note that <, >, " and ' are encoded, so the result should be safe to
+     * include directly in HTML.
+     *
+     * @param s A string to URL encode
+     * @return s with reserved characters % encoded using UTF-8 encoding.
+     */
+    public static String uriEnc(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            // This will never happen
+            throw new AssertionError("UTF-8 did not exist", e);
+        }
     }
 
     private ElFunctions() { throw new RuntimeException("No instantiation"); }
