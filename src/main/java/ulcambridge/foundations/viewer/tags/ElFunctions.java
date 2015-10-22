e1033d380906eaac68bf5fa6443fc93d66a2b945
Hal Blackburn <hwtb2@cam.ac.uk>
2015-10-22 12:40:15 +0100

    Add getItem EL function
    This is needed to interact with the ItemFactory in JSP templates
    (without using scriptlets).

--- src/main/java/ulcambridge/foundations/viewer/tags/ElFunctions.java
+++ src/main/java/ulcambridge/foundations/viewer/tags/ElFunctions.java
@@ -1,7 +1,9 @@
 package ulcambridge.foundations.viewer.tags;
 
 import ulcambridge.foundations.viewer.CollectionFactory;
+import ulcambridge.foundations.viewer.ItemFactory;
 import ulcambridge.foundations.viewer.model.Collection;
+import ulcambridge.foundations.viewer.model.Item;
 
 /**
  * This class defines custom JSP Expression Language (EL) functions required to
@@ -17,6 +19,13 @@ public final class ElFunctions {
      */
     public static Collection getCollection(CollectionFactory factory, String collectionId) {
         return factory.getCollectionFromId(collectionId);
+    }
+
+    /**
+     * @see ItemFactory#getItemFromId(String)
+     */
+    public static Item getItem(ItemFactory factory, String itemId) {
+        return factory.getItemFromId(itemId);
     }
 
     private ElFunctions() { throw new RuntimeException("No instantiation"); }
