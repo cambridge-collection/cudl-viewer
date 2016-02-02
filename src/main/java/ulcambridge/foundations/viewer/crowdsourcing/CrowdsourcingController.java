fe570d0dd35233da0cc9c581d795fc46c85d2a14
Hal Blackburn <hwtb2@cam.ac.uk>
2016-02-02 12:28:50 +0000

    Send 403 response from tagging endpoints on auth errors

--- src/main/java/ulcambridge/foundations/viewer/crowdsourcing/CrowdsourcingController.java
+++ src/main/java/ulcambridge/foundations/viewer/crowdsourcing/CrowdsourcingController.java
@@ -1,24 +1,19 @@
 package ulcambridge.foundations.viewer.crowdsourcing;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.sql.SQLException;
-import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.jena.riot.RDFFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import ulcambridge.foundations.viewer.crowdsourcing.dao.CrowdsourcingDao;
 import ulcambridge.foundations.viewer.crowdsourcing.model.Annotation;
 import ulcambridge.foundations.viewer.crowdsourcing.model.DocumentAnnotations;
@@ -31,6 +26,17 @@ import ulcambridge.foundations.viewer.crowdsourcing.model.UserAnnotations;
 import ulcambridge.foundations.viewer.rdf.RDFReader;
 import ulcambridge.foundations.viewer.utils.Utils;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 /**
  * Handles requests for the crowdsourcing platform.
  * 
@@ -40,17 +46,48 @@ import ulcambridge.foundations.viewer.utils.Utils;
 @RestController
 public class CrowdsourcingController {
 
+	private static final Set<String> USER_ROLES = Collections.unmodifiableSet(
+			new HashSet<String>(Arrays.asList("ROLE_USER", "ROLE_ADMIN")));
+
 	private static final Logger logger = LoggerFactory.getLogger(CrowdsourcingController.class);
 
 	private CrowdsourcingDao dataSource;
 
+	private final Set<String> allowedRoles;
+
+	public CrowdsourcingController() {
+		this.allowedRoles = USER_ROLES;
+	}
+
 	public CrowdsourcingController(CrowdsourcingDao dataSource) {
+		this();
 		this.dataSource = dataSource;
 	}
 
-	private boolean isUserAuthed() {
+	private boolean isUserAuthenticated() {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		return !auth.getName().equals("anonymousUser");
+
+		if(auth == null)
+			return false;
+
+		for(GrantedAuthority a : auth.getAuthorities()) {
+			if(this.allowedRoles.contains(a.getAuthority()))
+				return true;
+		}
+
+		return false;
+	}
+
+	private void ensureAuthenticated() {
+		if(!isUserAuthenticated())
+			throw new PermissionDeniedException();
+	}
+
+	private static class PermissionDeniedException extends RuntimeException { }
+
+	@ExceptionHandler(PermissionDeniedException.class)
+	private ResponseEntity<Void> handlePermissionDenied(PermissionDeniedException e ) {
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 	}
 
 	private JsonResponse redirect() {
@@ -62,12 +99,11 @@ public class CrowdsourcingController {
 
 	// on path /anno/get
 	@RequestMapping(value = "/anno/get/{docId}/{docPage}", method = RequestMethod.GET, produces = { "application/json" })
-	public JsonResponse handleAnnotationsFetch(@PathVariable("docId") String documentId, @PathVariable("docPage") int documentPageNo)
+	public JsonResponse handleAnnotationsFetch(@PathVariable("docId") String documentId, @PathVariable("docPage") int documentPageNo,
+											   HttpServletRequest req)
 			throws IOException {
 
-		// check user autentication
-		if (!isUserAuthed())
-			return redirect();
+		ensureAuthenticated();
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		DocumentAnnotations docAnnotations = dataSource.getAnnotations(auth.getName(), documentId, documentPageNo);
@@ -80,9 +116,7 @@ public class CrowdsourcingController {
 	public JsonResponse handleAnnotationAddOrUpdate(@PathVariable("docId") String documentId, @PathVariable("docPage") int documentPageNo,
 			@RequestBody Annotation annotation) throws SQLException, IOException {
 
-		// check user autentication
-		if (!isUserAuthed())
-			return redirect();
+		ensureAuthenticated();
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		int result = dataSource.addAnnotation(auth.getName(), documentId, annotation);
@@ -94,9 +128,7 @@ public class CrowdsourcingController {
 	public JsonResponse handleAnnotationRemove(@PathVariable("docId") String documentId, @PathVariable("uuid") String annotationUuid)
 			throws SQLException, IOException {
 
-		// check user autentication
-		if (!isUserAuthed())
-			return redirect();
+		ensureAuthenticated();
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		int result = dataSource.removeAnnotation(auth.getName(), documentId, UUID.fromString(annotationUuid));
@@ -121,9 +153,7 @@ public class CrowdsourcingController {
 	@RequestMapping(value = "/rmvtag/get/{docId}", method = RequestMethod.GET, produces = { "application/json" })
 	public JsonResponse handleRemovedTagsFetch(@PathVariable("docId") String documentId) throws IOException {
 
-		// check user autentication
-		if (!isUserAuthed())
-			return redirect();
+		ensureAuthenticated();
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		DocumentTags docTags = dataSource.getRemovedTags(auth.getName(), documentId);
@@ -136,9 +166,7 @@ public class CrowdsourcingController {
 	public JsonResponse handleRemovedTagAddOrUpdate(@PathVariable("docId") String documentId, @RequestBody Tag removedTag)
 			throws SQLException, IOException {
 
-		// check user autentication
-		if (!isUserAuthed())
-			return redirect();
+		ensureAuthenticated();
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		int result = dataSource.addRemovedTag(auth.getName(), documentId, removedTag);
@@ -149,9 +177,7 @@ public class CrowdsourcingController {
 	@RequestMapping(value = "/export", method = RequestMethod.GET, produces = { "application/rdf+xml" })
 	public void handleUserContributionsExport(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-		// check user autentication
-		if (!isUserAuthed())
-			return;
+		ensureAuthenticated();
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
@@ -181,8 +207,7 @@ public class CrowdsourcingController {
 	public void handleUserDocumentContributionsExport(@PathVariable("docId") String documentId, HttpServletRequest request,
 			HttpServletResponse response) throws IOException {
 
-		if (!isUserAuthed())
-			return;
+		ensureAuthenticated();
 
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
@@ -204,5 +229,4 @@ public class CrowdsourcingController {
 		response.flushBuffer();
 		os.close();
 	}
-
 }
