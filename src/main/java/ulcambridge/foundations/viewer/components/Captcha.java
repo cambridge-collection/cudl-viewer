9dcbd7e69eae661ae22e6e37949a6ee8e305a85f
James Howe <jmh205@cam.ac.uk>
2018-06-22 15:54:46 +0100

    CUDL-119: Replace captcha implementation with Google ReCAPTCHAv2.

---
+++ src/main/java/ulcambridge/foundations/viewer/components/Captcha.java
@@ -0,0 +1,57 @@
+package ulcambridge.foundations.viewer.components;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.util.HtmlUtils;
+
+import java.util.Map;
+
+@Component
+public class Captcha {
+    private final static String RESPONSE_PARAM = "g-recaptcha-response";
+    private final static String VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify";
+    private final String siteKey;
+    private final String secretKey;
+    private final RestOperations operations;
+
+    public Captcha(
+        @Value("${recaptcha.siteKey}") String siteKey,
+        @Value("${recaptcha.secretKey}")String secretKey,
+        @Autowired RestOperations operations
+    ) {
+        this.siteKey = siteKey;
+        this.secretKey = secretKey;
+        this.operations = operations;
+    }
+
+    public String getHtml() {
+        return String.format(
+            "<script src='https://www.google.com/recaptcha/api.js' async='false'></script>" +
+            "<div class='g-recaptcha' data-sitekey='%s'></div>",
+            HtmlUtils.htmlEscape(this.siteKey)
+        );
+    }
+
+    public String getResponseParam() {
+        return RESPONSE_PARAM;
+    }
+
+    public boolean verify(String responseToken) {
+        return this.verify(responseToken, null);
+    }
+
+    public boolean verify(String responseToken, String remoteAddr) {
+        MultiValueMap<String, String> request = new LinkedMultiValueMap<>();
+        request.add("secret", this.secretKey);
+        request.add("response", responseToken);
+        if (remoteAddr != null) {
+            request.add("remoteip", remoteAddr);
+        }
+        Map response = operations.postForObject(VERIFY_URL, request, Map.class);
+        return Boolean.TRUE == response.get("success");
+    }
+}
