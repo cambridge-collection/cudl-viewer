a02460a64fab90c241bb201ad8a10ee8d771f7e0
Hal Blackburn <hwtb2@cam.ac.uk>
2018-10-02 10:07:47 +0100

    Move session timeout out of web.xml

--- src/main/java/ulcambridge/foundations/viewer/ViewerApplication.java
+++ src/main/java/ulcambridge/foundations/viewer/ViewerApplication.java
@@ -1,14 +1,19 @@
 package ulcambridge.foundations.viewer;
 
+import com.google.common.collect.ImmutableMap;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ViewerApplication {
 
     public static void main(String[] args) {
-        new SpringApplication(ViewerApplication.class)
-            .run(args);
+        SpringApplication app = new SpringApplication(ViewerApplication.class);
+
+        // TODO: Move this to default configuration file
+        app.setDefaultProperties(ImmutableMap.of(
+                "server.servlet.session.timeout", 240));
+
+        app.run(args);
     }
 }
