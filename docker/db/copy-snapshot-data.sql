8eda2184b082a5f81236fb0f54129f47b25a51f6
Hal Blackburn <hwtb2@cam.ac.uk>
2020-02-13 12:45:17 +0000

    WIP: docker-compose support

---
+++ docker/db/copy-snapshot-data.sql
@@ -0,0 +1,3 @@
+COPY collections FROM '/srv/cudl-viewer/db-dump/collections.txt' WITH ENCODING 'UTF8';
+COPY items FROM '/srv/cudl-viewer/db-dump/items.txt' WITH ENCODING 'UTF8';
+COPY itemsincollection FROM '/srv/cudl-viewer/db-dump/itemsincollection.txt' WITH ENCODING 'UTF8';
