c325e106d244dd9b6d65890c56b28d032994ad35
Hal Blackburn <hwtb2@cam.ac.uk>
2020-02-18 13:03:34 +0000

    Register a MockMvc bean for use by tests

--- src/test/java/ulcambridge/foundations/viewer/testing/BaseCUDLApplicationContextTest.java
+++ src/test/java/ulcambridge/foundations/viewer/testing/BaseCUDLApplicationContextTest.java
@@ -1,11 +1,16 @@
 package ulcambridge.foundations.viewer.testing;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import ulcambridge.foundations.viewer.config.AppConfig;
 import ulcambridge.foundations.viewer.config.DispatchServletConfig;
 
@@ -21,15 +26,23 @@ import ulcambridge.foundations.viewer.config.DispatchServletConfig;
 @ExtendWith(SpringExtension.class)
 @WebAppConfiguration
 @ContextHierarchy({
-        @ContextConfiguration(name = "parent", classes = {
-                AppConfig.class,
-                ParentTestConfig.class
-        }),
-        @ContextConfiguration(name = "child", classes = {
-                DispatchServletConfig.class,
-                ChildTestConfig.class
-        })
+    @ContextConfiguration(name = "parent", classes = {
+        AppConfig.class,
+        ParentTestConfig.class
+    }),
+    @ContextConfiguration(name = "child", classes = {
+        DispatchServletConfig.class,
+        BaseCUDLApplicationContextTest.TestSpecificBeanConfig.class,
+        ChildTestConfig.class
+    })
 })
 @ActiveProfiles("test")
 public abstract class BaseCUDLApplicationContextTest {
+    public static class TestSpecificBeanConfig {
+        @Bean
+        @Lazy
+        public MockMvc mockMvc(WebApplicationContext wac) {
+            return MockMvcBuilders.webAppContextSetup(wac).build();
+        }
+    }
 }
