6101b7a6641826ef0d52d27fe8f1717e6b305aeb
Rory McHugh <rmm98@cam.ac.uk>
2024-10-24 16:11:17 +0100

    feat(codebuild): Update CodeBuild documentation
    - Add new sections to README.md
    - Update terraform/terraform.tf to use Terraform version 1.9.7
    - Remove aws_ecr_repository.cudl_viewer_db resource
    - Remove reference to aws_ecr_repository.cudl_viewer_db.arn
      in aws_iam_policy_document.code_build
    - Update value of codebuild_git_source_version to refer to main
      branch

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,8 +2,3 @@ resource "aws_ecr_repository" "cudl_viewer" {
   name         = join("-", [var.tag_environment, "cudl-viewer"])
   force_delete = var.ecr_repository_force_delete
 }
-
-resource "aws_ecr_repository" "cudl_viewer_db" {
-  name         = join("-", [var.tag_environment, "cudl-viewer-db"])
-  force_delete = var.ecr_repository_force_delete
-}
